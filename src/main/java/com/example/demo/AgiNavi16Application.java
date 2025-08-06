b8b7c2cb08798b377c103345587f1111e21ede12
KeisukeHoshino <keisuke.hoshino.pl@gmail.com>
2025-08-06 17:00:00 +0900

    first commit

---
+++ src/main/java/com/example/demo/AgiNavi16Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgiNavi16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgiNavi16Application.class, args);
+	}
+
+}
