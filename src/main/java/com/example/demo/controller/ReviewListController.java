009704db0315c131ae1ff9c39a3e6861c48d74a6
KeisukeHoshino <keisuke.hoshino.pl@gmail.com>
2025-08-12 21:31:37 +0900

    レビュー一覧検索機能追加

--- src/main/java/com/example/demo/controller/ReviewListController.java
+++ src/main/java/com/example/demo/controller/ReviewListController.java
@@ -1,7 +1,5 @@
 package com.example.demo.controller;
 
-import java.sql.Date;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -11,9 +9,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.entity.Review;
 import com.example.demo.form.ReviewSearchForm;
+import com.example.demo.service.ReviewListService;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class ReviewListController {
+	
+	private final ReviewListService service;
 
 	/**
 	 * 検索リクエスト
@@ -24,26 +28,8 @@ public class ReviewListController {
 	@PostMapping("/search-review")
 	private String searchReview (@ModelAttribute ReviewSearchForm form, Model model) {
 		
-		List<Review> list = new ArrayList<Review>();
 		
-//		テストデータ
-		Review review = new Review();
-		review.setReviewId(1);
-		review.setRestaurantId(1);
-		review.setUserId("user1");
-		review.setVisitDate(Date.valueOf("2027-07-01"));
-		review.setRating(3);
-		review.setComment("comment1");
-		list.add(review);
-		
-		review = new Review();
-		review.setReviewId(2);
-		review.setRestaurantId(1);
-		review.setUserId("user2");
-		review.setVisitDate(Date.valueOf("2027-07-02"));
-		review.setRating(4);
-		review.setComment("comment2");
-		list.add(review);
+		List<Review> list = service.findByRestaurantId(form.getRestaurantId());
 		
 		if(list.size() > 0) {
 			model.addAttribute("reviewList", list);
