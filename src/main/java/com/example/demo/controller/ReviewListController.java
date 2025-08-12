721bf4bc3f11c9ddd221419fc80882c9382097f9
KeisukeHoshino <keisuke.hoshino.pl@gmail.com>
2025-08-12 16:16:46 +0900

    レビュー一覧画面作成

---
+++ src/main/java/com/example/demo/controller/ReviewListController.java
@@ -0,0 +1,54 @@
+package com.example.demo.controller;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.entity.Review;
+import com.example.demo.form.ReviewSearchForm;
+
+@Controller
+public class ReviewListController {
+
+	/**
+	 * 検索リクエスト
+	 * @param form
+	 * @param model
+	 * @return
+	 */
+	@PostMapping("/search-review")
+	private String searchReview (@ModelAttribute ReviewSearchForm form, Model model) {
+		
+		List<Review> list = new ArrayList<Review>();
+		
+//		テストデータ
+		Review review = new Review();
+		review.setReviewId(1);
+		review.setRestaurantId(1);
+		review.setUserId("user1");
+		review.setVisitDate(Date.valueOf("2027-07-01"));
+		review.setRating(3);
+		review.setComment("comment1");
+		list.add(review);
+		
+		review = new Review();
+		review.setReviewId(2);
+		review.setRestaurantId(1);
+		review.setUserId("user2");
+		review.setVisitDate(Date.valueOf("2027-07-02"));
+		review.setRating(4);
+		review.setComment("comment2");
+		list.add(review);
+		
+		if(list.size() > 0) {
+			model.addAttribute("reviewList", list);
+		}
+
+		return "review-list";
+	}
+}
