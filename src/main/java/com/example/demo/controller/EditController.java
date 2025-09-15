511bfbafbae7d60b68c4190b9f61bb01796ec27e
KeisukeHoshino <keisuke.hoshino.pl@gmail.com>
2025-09-15 17:43:59 +0900

    編集でDBの値を更新できるように修正

--- src/main/java/com/example/demo/controller/EditController.java
+++ src/main/java/com/example/demo/controller/EditController.java
@@ -7,13 +7,17 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.demo.entity.Review;
 import com.example.demo.form.ReviewEditForm;
+import com.example.demo.service.EditService;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 public class EditController {
+	
+	private final EditService service;
 
 //	レビュー編集画面表示リクエスト
 	@PostMapping("/show-edit-form")
@@ -43,18 +47,15 @@ public class EditController {
 			return "edit-review";
 		}
 
-		ReviewEditForm r = new ReviewEditForm();
+		Review r = new Review();
 		r.setReviewId(form.getReviewId());
 		r.setRestaurantId(form.getRestaurantId());
-		r.setRestaurantName(form.getRestaurantName());
 		r.setUserId(form.getUserId());
 		r.setVisitDate(form.getVisitDate());
 		r.setRating(form.getRating());
 		r.setComment(form.getComment());
-		
-//		暫定で表示
-		System.out.println("--レヴュー更新");
-		System.out.println(r);
+
+		service.edit(r);
 		
 		redirectAttributes.addFlashAttribute("msg", "（レビュー更新）");
 		
